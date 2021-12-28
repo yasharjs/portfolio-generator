fcb447dd9b33a59e90d66c1e7894e025e4dc5f4c
Yashar Sarabi <77018659+yasharjs@users.noreply.github.com>
2021-12-27 20:09:39 -0500

    final site

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
+const {writeFile, copyFile} = require('./utils/generate-site.js')
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
@@ -133,12 +133,18 @@ const promptProject = portfolioData =>{
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html',pageHTML, err =>{
-        if(err) throw new Error(err);
-
-        console.log("Portfolio complete! Check out index.html to see the output!");
-    })
-  });
-
+      return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+      return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+      console.log(writeFileResponse);
+      return copyFile();
+  })
+  .then(copyFileResponse => {
+      console.log(copyFileResponse);
+  })
+  .catch(err => {
+      console.log(err);
+  })
